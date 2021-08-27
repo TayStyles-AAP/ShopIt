df840201abd5c342ed9118219c349cc5ee415c3d
beny098 <83893615+beny098@users.noreply.github.com>
2021-08-27 12:04:44 +1200

    Update ResetActivity and ResetPasswordActivity

--- app/src/main/java/com/example/shopit/ui/reset/ResetActivity.kt
+++ app/src/main/java/com/example/shopit/ui/reset/ResetActivity.kt
@@ -1,5 +1,6 @@
 package com.example.shopit.ui.reset
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -8,9 +9,44 @@ import com.example.shopit.R
 
 class ResetActivity : AppCompatActivity() {
 
+    //Add Ui elements as variables with types of elements
+    lateinit var emailTextInput: EditText
+    lateinit var sendReset: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset)
 
+        //initilise the variables
+        emailTextInput = findViewById(R.id.editTextTextEmailAddressResetPassword)
+        sendReset = findViewById(R.id.buttonResetPassword)
+
+
+        sendReset.setOnClickListener {
+            if (validateEmail()) {
+                //transition to Reset Password Activity
+                startActivity(Intent(this, ResetPasswordActivity::class.java))
+                finish() //This means that if the user pressed the back button on the OS,
+                        // it will not be able to navigate back to this screen
+            }
+
+
+        }
+    }
+
+    private fun validateEmail(): Boolean {
+        val email = emailTextInput.text.toString()
+
+        return if (email.isEmpty()) {
+            emailTextInput.error = "Please enter email address!"
+            false
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailTextInput.error = "Please check your email!"
+            false
+        } else {
+            true
+        }
     }
 }
---
+++ app/src/main/java/com/example/shopit/ui/reset/ResetPasswordActivity.kt
@@ -0,0 +1,17 @@
+package com.example.shopit.ui.reset
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shopit.MainActivity
+import com.example.shopit.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reset_password_new_password)
+    }
+}
