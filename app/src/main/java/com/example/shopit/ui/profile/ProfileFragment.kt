375d6970fc64f7141c317ad2b630af5d0d0cc04a
JackwillJudge <jack.w.judge@gmail.com>
2021-10-17 22:50:44 +1300

    Edit profile functionality complete

--- app/src/main/java/com/example/shopit/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/shopit/ui/profile/ProfileFragment.kt
@@ -6,17 +6,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.shopit.MainActivity
 import com.example.shopit.R
 import com.example.shopit.ui.login.LoginActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -39,6 +42,7 @@ class ProfileFragment : Fragment() {
     lateinit var newPasswordInput: EditText
     lateinit var newPasswordRepeatInput: EditText
     private val picasso: Picasso = Picasso.get()
+    lateinit var saveButton : Button
 
     lateinit var logoutButton: FloatingActionButton
 
@@ -62,7 +66,20 @@ class ProfileFragment : Fragment() {
         newPasswordInput = view.findViewById(R.id.edit_profile_new_password)
         newPasswordRepeatInput = view.findViewById(R.id.edit_profile_new_password_repeat)
 
+        saveButton = view.findViewById(R.id.edit_profile_save_button)
+
         getProfileData()
+
+        saveButton.setOnClickListener {
+            editProfileData{ submitWasSuccess ->
+                if (submitWasSuccess) {
+                    Snackbar.make(requireView(), "Successfully Updated Profile", Snackbar.LENGTH_SHORT).show()
+                } else {
+                    Snackbar.make(requireView(), "Failed To Update Profile", Snackbar.LENGTH_SHORT).show()
+                }
+
+            }
+        }
 
         view.findViewById<FloatingActionButton>(R.id.logoutButton).setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -120,6 +137,34 @@ class ProfileFragment : Fragment() {
                         lastnameEditText.setText(lastName)
                         emailEditText.setText(email)
                     }
+                }
+        }
+    }
+
+    private fun editProfileData(completion: (Boolean) -> Unit){
+        val currentUser = Firebase.auth.currentUser
+
+        if (currentUser != null) {
+            FirebaseFirestore.getInstance().collection("Users")
+                .document(currentUser.uid).get()
+                .addOnCompleteListener { task ->
+                    val document = task.result
+                    val firstName = firstnameEditText.getText().toString()
+                    val lastName = lastnameEditText.getText().toString()
+                    val email = emailEditText.getText().toString()
+
+                    val ref = FirebaseFirestore.getInstance().collection("Users").document(currentUser.uid)
+                    FirebaseFirestore.getInstance().runBatch { batch ->
+                        batch.update(ref, "first_name", firstName)
+                        batch.update(ref, "last_name", lastName)
+                        batch.update(ref,"email", email)
+                    }
+                        .addOnCompleteListener {
+                            completion(true) }
+
+                        .addOnFailureListener {
+                            completion(false)
+                        }
                 }
         }
     }
